eea89fbb3d315aa89ed2b070f90c60a0cc922aaf
Mohammed Nifli <mohammednifliap@gmail.com>
2024-12-26 20:20:03 +0530

    fix:env config issue

--- src/server.ts
+++ src/server.ts
@@ -140,7 +140,7 @@ io.on('connection', (socket) => {
 
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4444;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
