8553f0af020583a2556455d0fa9eff35b17dbbda
MohammedNifli <148385105+MohammedNifli@users.noreply.github.com>
2025-01-03 13:46:05 +0530

    Delete dist directory

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// start.ts
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-register('ts-node/esm', pathToFileURL('./'));
-// Now import your main application file
-import './src/index.ts'; // Adjust the path to your main file
