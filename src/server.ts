21ddb4ac081f297a9b0125a5e08ce0f9ad75b85b
Mohammed Nifli <mohammednifliap@gmail.com>
2025-01-03 06:49:11 +0530

    fix:change the domain

--- src/server.ts
+++ src/server.ts
@@ -69,7 +69,7 @@ app.use(cookieParser());
 
 // CORS options for allowing cookies and specifying allowed origins
 const corsOptions = {
-  origin: " http://localhost:5173", 
+  origin: "med-tech-connect.vercel.app", 
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], 
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
